c16482759a4148429b82355798bad66620d5f12d
Jesse Clark <jesse.clark@unco.edu>
2017-03-03 11:13:19 -0700

    OO code to get sha1 from string.

---
+++ BlockchainHelloWorld/Encryption.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security.Cryptography;
+using System.Text;
+using System.Web;
+
+namespace BlockchainHelloWorld {
+    public class Encryption {
+
+
+        public string Encrypt(String s) {
+            return ConvertToString(Sha1(s)); ;
+        }
+
+        /// <summary>
+        /// Convert String to sha1
+        /// </summary>
+        /// <param name="s"></param>
+        /// <returns></returns>
+        private byte[] Sha1(String s) {
+
+            // convert string to bytes:
+            byte[] data = Encoding.UTF8.GetBytes(s);
+
+            // create sha instance:
+            SHA1 sha = new SHA1CryptoServiceProvider();
+            
+            // return computedhash of s
+            return sha.ComputeHash(data);
+        }
+
+        /// <summary>
+        /// Converts byte[] to String of sha1
+        /// </summary>
+        /// <param name="result">Hash of the incoming string</param>
+        /// <returns></returns>
+        private String ConvertToString(byte[] result) {
+            var sb = new StringBuilder();
+            foreach (byte b in result) {
+                var hex = b.ToString("x2");
+                sb.Append(hex);
+            }
+            return sb.ToString();
+        }
+    }
+}
--- BlockchainHelloWorldTest/EncryptionTest.cs
+++ BlockchainHelloWorldTest/EncryptionTest.cs
@@ -2,6 +2,7 @@
 using Microsoft.VisualStudio.TestTools.UnitTesting;
 using System.Security.Cryptography;
 using System.Text;
+using BlockchainHelloWorld;
 
 namespace BlockchainHelloWorldTest {
     [TestClass]
@@ -16,7 +17,6 @@ namespace BlockchainHelloWorldTest {
         /// </summary>
         [TestMethod]
         public void TestBasicCrypto() {
-
 
             //byte[] data = new byte[256];
             byte[] data = Encoding.UTF8.GetBytes("Hello World");
@@ -37,5 +37,16 @@ namespace BlockchainHelloWorldTest {
             Assert.AreEqual("0a4d55a8d778e5022fab701977c5d840bbc486d0", sb.ToString());
 
         }
+
+
+        [TestMethod]
+        public void TestEncryptString() {
+
+            Encryption e = new Encryption();
+            String result = e.Encrypt("Hello World");
+
+            Assert.AreEqual("0a4d55a8d778e5022fab701977c5d840bbc486d0", result );
+        }
+
     }
 }
