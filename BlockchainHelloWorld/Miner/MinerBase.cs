1ca087e70ddf8f129fcccab300c3f78bbb288c2b
Jesse Clark <jesse.clark@unco.edu>
2017-03-03 14:37:46 -0700

    Write the first simple miner to start solving blocks!

---
+++ BlockchainHelloWorld/Miner/MinerBase.cs
@@ -0,0 +1,79 @@
+﻿using BlockchainHelloWorld.Block.Encryption;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace BlockchainHelloWorld.Block.Miner {
+    public class MinerBase {
+
+        private BlockBase Block;
+        private int Difficulty;
+
+
+        /// <summary>
+        /// Setters
+        /// </summary>
+        public void SetBlock(BlockBase b){
+            Block = b;
+        }
+
+        public void SetDifficulty(int i) {
+            Difficulty = i;
+        }
+
+
+        /// <summary>
+        /// Getters
+        /// </summary>
+        public BlockBase GetBlock() {
+            return Block;
+        }
+
+
+
+
+        public int Mine(int starting = 0, int ending = 1000000) {
+
+            EncryptionBlock e = new EncryptionBlock();
+
+            for (int i = starting; i <= 1000000; i++) {
+                
+                //Set the nonce on the block:
+                Block.SetNonce(i);
+
+                //hash the block:
+                String hash = e.Encrypt(Block);
+
+                if (CompareHashAgainstDifficulty(hash))
+                    return i;
+            }
+
+            // no result found:
+            throw new Exception("No result found between [" + starting + "]-[" + ending + "] with a difficulty of [" + Difficulty + "]");
+
+        }
+
+
+        /// <summary>
+        /// Check the hash against the difficulty. Right now the difficulty level is done with leading zeros
+        /// 1 = 0, 2 = 00, 3 = 000, etc. 
+        /// @todo: make this a bit more sophisticated
+        /// </summary>
+        /// <param name="hash"></param>
+        /// <returns></returns>
+        private bool CompareHashAgainstDifficulty(String hash) {
+
+            string stringDifficulty = "";
+            for (int i = 0; i < Difficulty; i++) {
+                stringDifficulty += "0";
+            }
+
+            if (hash.StartsWith(stringDifficulty))
+                return true;
+
+            return false;
+        }
+
+    }
+}
---
+++ BlockchainHelloWorldTest/Miner/MinerTest.cs
@@ -0,0 +1,69 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using BlockchainHelloWorld.Block.Miner;
+using BlockchainHelloWorld.Block;
+using BlockchainHelloWorld.Block.Encryption;
+
+namespace BlockchainHelloWorldTest.Miner {
+    [TestClass]
+    public class MinerTest {
+
+        BlockBase b1;
+        MinerBase mb;
+
+        [TestInitialize()]
+        public void Initialize() {
+            b1 = new BlockBase(1);
+            mb = new MinerBase();
+            mb.SetBlock(b1);
+        }
+
+        [TestMethod]
+        public void TestMinerBlockBase() {
+
+            Assert.AreEqual(b1, mb.GetBlock());
+
+            Assert.AreEqual(
+                b1.GetId(),
+                mb.GetBlock().GetId()
+                );
+
+        }
+
+        [TestMethod]
+        public void TestMinerBlockBaseMine() {
+
+            mb.SetDifficulty(1);
+
+            int nonce = mb.Mine();
+
+            // Assert the nonce:
+            Assert.AreEqual(19, nonce);
+
+            // Is the nonce set on the block?
+            Assert.AreEqual(b1.GetNonce(), nonce);
+
+            // Check the hash's starting 0:
+            EncryptionBlock e = new EncryptionBlock();
+            String hash = e.Encrypt(b1);
+            Assert.AreEqual("06c24c8fe5e39f038a3570e562fc279e37af97d3", hash);
+
+        }
+
+        [TestMethod]
+        public void TestMinerBaseDifficulty() {
+
+            mb.SetDifficulty(2);
+            int nonce = mb.Mine();
+
+            Assert.AreEqual(90, nonce);
+
+            // Let's see the Hash:
+            EncryptionBlock e = new EncryptionBlock();
+            String hash = e.Encrypt(b1);
+            Assert.AreEqual("0086f15ccc3f3164004aeff5792c3e054d746d5a", hash);
+
+        }
+
+    }
+}
